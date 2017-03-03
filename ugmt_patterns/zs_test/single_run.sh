0adf623a8c85ed0796193403f5c390398607f90d
Thomas Reis <thomas.reis@cern.ch>
2017-03-03 16:47:14 +0100

    Same RO and ZS menu as at P5. Add intermediates to test.

--- ugmt_patterns/zs_test/single_run.sh
+++ ugmt_patterns/zs_test/single_run.sh
@@ -21,22 +21,25 @@ mkdir $work_path/error_events
 
 printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" | cat - /home/utcausr/test_patterns/TT_13TeV_RunIIFall15DR76-25nsFlat10to25TSG/rx_TT_13TeV_RunIIFall15DR76-25nsFlat10to25TSG_18.zip | gzip -dc > $tmp_path/uncompressed.txt
 	
-#mp7butler.py -c $CONN reset $address --clksrc=internal # comment this line if the ZS is configured with SWATCH
+mp7butler.py -c $CONN reset $address --clksrc=internal # comment this line if the ZS is configured with SWATCH
+#mp7butler.py -c $CONN reset $address --clksrc=external # comment this line if the ZS is configured with SWATCH
 	
 mp7butler.py -c $CONN -v xbuffers $address tx PlayOnce -e $e --inject file://$tmp_path/uncompressed.txt 
 mp7butler.py -c $CONN txmgts -e $e --loopback --pattern=none $address
 mp7butler.py -c $CONN rxmgts -e $e $address
 mp7butler.py -c $CONN rxalign -e $e --to-bx 7,5 $address
-mp7butler.py -c $CONN easylatency $address --txBank 2 --tx 0-3 --rxBank 1 --rx 36-71 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12
+mp7butler.py -c $CONN easylatency $address --txBank 2 --tx 0-3,24-29 --rxBank 1 --rx 36-71 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12 # MP7 FW 2.2.1 has a bug that only a limited number of links is supported with ZS. The limit seems to be 46 which means that only 6 of 8 intermediate muons can be in the RO
 mp7butler.py -c $CONN rosetup $address --internal --bxoffset 2
+#mp7butler.py -c $CONN rosetup $address --bxoffset 2
 mp7butler.py -c $CONN romenu $address $ROMENU_FILE standardMenu
-#mp7butler.py -c $CONN zsmenu $address $ROMENU_FILE zsStandardMenu # comment this line if the ZS is configured with SWATCH
+mp7butler.py -c $CONN zsmenu $address $ROMENU_FILE zsStandardMenu # comment this line if the ZS is configured with SWATCH
 #bash setup_zs.sh
 	
 echo Pattern:	single >> $work_path/summary.txt
 echo Bx:	Status:  >> $work_path/summary.txt
 
 for i in {14..132}
+#for i in {14..15}
 do
 	mp7butler.py -c $CONN write $address readout.readout_zs.csr.ctrl.en 0 &>/dev/null
 	mp7butler.py -c $CONN -v roevents $address 1 --bxs $i --outputpath $tmp_path/output_nozs.dat &>/dev/null
