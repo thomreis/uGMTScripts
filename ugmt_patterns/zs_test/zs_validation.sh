756284fc504abac35b57a82651b798513ba8d86f
Thomas Reis <thomas.reis@cern.ch>
2017-09-21 18:19:54 +0200

    Replace tabs with spaces.

--- ugmt_patterns/zs_test/zs_validation.sh
+++ ugmt_patterns/zs_test/zs_validation.sh
@@ -21,51 +21,51 @@ python $root_path/paths/lists_tx_rx.py
 #loop over $patterns
 while read rxfile
 do
-	rm $tmp_path/uncompressed.txt
+  rm $tmp_path/uncompressed.txt
 
-	#magic method to unpack files zipped in python
-	printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" | cat - $rxfile | gzip -dc > $tmp_path/uncompressed.txt
+  #magic method to unpack files zipped in python
+  printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" | cat - $rxfile | gzip -dc > $tmp_path/uncompressed.txt
 
-	#mp7butler.py -c $CONN reset $address --clksrc=internal # comment this line if the ZS is configured with SWATCH
+  #mp7butler.py -c $CONN reset $address --clksrc=internal # comment this line if the ZS is configured with SWATCH
 
-	#inject uncompressed rx file
-	mp7butler.py -c $CONN -v xbuffers $address tx PlayOnce -e $e --inject file://$tmp_path/uncompressed.txt
-	mp7butler.py -c $CONN txmgts -e $e --loopback --pattern=none $address
-	mp7butler.py -c $CONN rxmgts -e $e $address
-	mp7butler.py -c $CONN rxalign -e $e --to-bx 7,5 $address
-	mp7butler.py -c $CONN easylatency $address --txBank 2 --tx 0-23,24-31 --rxBank 1 --rx 36-71 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12
-	mp7butler.py -c $CONN rosetup $address --internal --bxoffset 2
-	mp7butler.py -c $CONN romenu $address $ROMENU_FILE standardMenu
-	#mp7butler.py -c $CONN zsmenu $address $ROMENU_FILE zsStandardMenu # comment this line if the ZS is configured with SWATCH
-	#bash setup_zs.sh
+  #inject uncompressed rx file
+  mp7butler.py -c $CONN -v xbuffers $address tx PlayOnce -e $e --inject file://$tmp_path/uncompressed.txt
+  mp7butler.py -c $CONN txmgts -e $e --loopback --pattern=none $address
+  mp7butler.py -c $CONN rxmgts -e $e $address
+  mp7butler.py -c $CONN rxalign -e $e --to-bx 7,5 $address
+  mp7butler.py -c $CONN easylatency $address --txBank 2 --tx 0-23,24-31 --rxBank 1 --rx 36-71 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12
+  mp7butler.py -c $CONN rosetup $address --internal --bxoffset 2
+  mp7butler.py -c $CONN romenu $address $ROMENU_FILE standardMenu
+  mp7butler.py -c $CONN zsmenu $address $ROMENU_FILE zsStandardMenu # comment this line if the ZS is configured with SWATCH
+  #bash setup_zs.sh
 
-	echo Pattern:	$rxfile >> $work_path/summary.txt
-	echo Bx:	Status: >> $work_path/summary.txt
+  echo Pattern:  $rxfile >> $work_path/summary.txt
+  echo Bx:       Status: >> $work_path/summary.txt
 
-	for i in {14..132}
-	do
-		#disable zs menu
-		mp7butler.py -c $CONN write $address readout.readout_zs.csr.ctrl.en 0 &>/dev/null
-		mp7butler.py -c $CONN -v roevents $address 1 --bxs $i --outputpath $tmp_path/output_nozs.dat &>/dev/null
+  for i in {14..132}
+  do
+    #disable zs menu
+    mp7butler.py -c $CONN write $address readout.readout_zs.csr.ctrl.en 0 &>/dev/null
+    mp7butler.py -c $CONN -v roevents $address 1 --bxs $i --outputpath $tmp_path/output_nozs.dat &>/dev/null
 
-		#enable zs menu and check whether it is enabled (there were some problems with this)
-		mp7butler.py -c $CONN write $address readout.readout_zs.csr.ctrl.en 1 &>/dev/null
-		en="$(mp7butler.py -c $CONN -l inspect $address readout.readout_zs.csr.ctrl.en)"
+    #enable zs menu and check whether it is enabled (there were some problems with this)
+    mp7butler.py -c $CONN write $address readout.readout_zs.csr.ctrl.en 1 &>/dev/null
+    en="$(mp7butler.py -c $CONN -l inspect $address readout.readout_zs.csr.ctrl.en)"
 
-		until [ ${en:(-1)} == 1 ]
-		do
-			mp7butler.py -c $CONN write $address readout.readout_zs.csr.ctrl.en 1 &>/dev/null
-			#mp7butler.py -c $CONN inspect $address readout.readout_zs.csr.ctrl.val_mode
-			en="$(mp7butler.py -c $CONN -l inspect $address readout.readout_zs.csr.ctrl.en)"
-			echo Zero suppression status ${en:(-1)}
-		done
+    until [ ${en:(-1)} == 1 ]
+    do
+      mp7butler.py -c $CONN write $address readout.readout_zs.csr.ctrl.en 1 &>/dev/null
+      #mp7butler.py -c $CONN inspect $address readout.readout_zs.csr.ctrl.val_mode
+      en="$(mp7butler.py -c $CONN -l inspect $address readout.readout_zs.csr.ctrl.en)"
+      echo Zero suppression status ${en:(-1)}
+    done
 
-		mp7butler.py -c $CONN -v roevents $address 1 --bxs $i --outputpath $tmp_path/output_zs.dat &>/dev/null
+    mp7butler.py -c $CONN -v roevents $address 1 --bxs $i --outputpath $tmp_path/output_zs.dat &>/dev/null
 
-		python $root_path/check_zs_mask_new.py $tmp_path/output_nozs.dat $tmp_path/output_zs.dat $work_path $i $rxfile
-		echo Bx $i/132 complete
-	done
-	echo Pattern $rxfile completed
+    python $root_path/check_zs_mask_new.py $tmp_path/output_nozs.dat $tmp_path/output_zs.dat $work_path $i $rxfile
+    echo Bx $i/132 complete
+  done
+  echo Pattern $rxfile completed
 done < $patterns
 
 #script to do short summary in summary.txt file
