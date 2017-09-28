6e7e1774ec7d38cd81eca184ea4e1a28bc1747a4
Thomas Reis <thomas.reis@cern.ch>
2017-09-28 15:56:03 +0200

    Add increased timeout to avoid validation errors.

--- ugmt_patterns/zs_test/single_run.sh
+++ ugmt_patterns/zs_test/single_run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 CONN=/home/utcausr/mp7sw/connection_files/connections-b40.xml
-address=ugmt
+#address=ugmt
+address=ugmt-zs-sept2017
+TIMEOUT=5000
 root_path=/home/utcausr/uGMTScripts/ugmt_patterns/zs_test
 work_path=$root_path/single_run_`date +%Y%m%d_%H%M%S`
 tmp_path=$root_path/tmp
@@ -16,26 +18,26 @@ mkdir $work_path
 mkdir $work_path/error_events
 
 #printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" | cat - /home/utcausr/test_patterns/TT_13TeV_RunIIFall15DR76-25nsFlat10to25TSG/rx_TT_13TeV_RunIIFall15DR76-25nsFlat10to25TSG_18.zip | gzip -dc > $tmp_path/uncompressed.txt
-printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" | cat - /home/utcausr/test_patterns/TT_TuneCUETP8M1_13TeV/rx_TT_TuneCUETP8M1_13TeV_8.zip | gzip -dc > $tmp_path/uncompressed.txt
-#printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" | cat - /home/utcausr/test_patterns/JPsiToMuMu_Pt20to120_EtaPhiRestricted/rx_JPsiToMuMu_Pt20to120_EtaPhiRestricted_1.zip | gzip -dc > $tmp_path/uncompressed.txt
+#printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" | cat - /home/utcausr/test_patterns/TT_TuneCUETP8M1_13TeV/rx_TT_TuneCUETP8M1_13TeV_8.zip | gzip -dc > $tmp_path/uncompressed.txt
+printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" | cat - /home/utcausr/test_patterns/JPsiToMuMu_Pt20to120_EtaPhiRestricted/rx_JPsiToMuMu_Pt20to120_EtaPhiRestricted_2.zip | gzip -dc > $tmp_path/uncompressed.txt
 
 mp7butler.py -c $CONN reset $address --clksrc=internal # comment this line if the ZS is configured with SWATCH
 #mp7butler.py -c $CONN reset $address --clksrc=external # comment this line if the ZS is configured with SWATCH
 
-mp7butler.py -c $CONN -v xbuffers $address tx PlayOnce -e $e --inject file://$tmp_path/uncompressed.txt 
+mp7butler.py -c $CONN -v xbuffers $address --timeout $TIMEOUT tx PlayOnce -e $e --inject file://$tmp_path/uncompressed.txt 
 mp7butler.py -c $CONN txmgts -e $e --loopback --pattern=none $address
 mp7butler.py -c $CONN rxmgts -e $e $address
 mp7butler.py -c $CONN rxalign -e $e --to-bx 7,5 $address
 # MP7 FW 2.2.1 has a bug that only a limited number of links is supported with ZS.
 # The limit seems to be 46 which means that only 6 of 8 intermediate muons can be in the RO
-mp7butler.py -c $CONN easylatency $address --txBank 2 --tx 0-23,24-31 --rxBank 1 --rx 36-71 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12
-#mp7butler.py -c $CONN easylatency $address --txBank 2 --tx 0-71 --rxBank 1 --rx 0-71 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12
-#mp7butler.py -c $CONN easylatency $address --txBank 2 --tx 1 --rxBank 1 --rx 0-3 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12
-#mp7butler.py -c $CONN easylatency $address --txBank 2 --tx 31 --rxBank 1 --rx 50 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12
-mp7butler.py -c $CONN rosetup $address --internal --bxoffset 2
+mp7butler.py -c $CONN easylatency $address --timeout $TIMEOUT --txBank 2 --tx 0-23,24-31 --rxBank 1 --rx 36-71 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12
+#mp7butler.py -c $CONN easylatency $address --timeout $TIMEOUT --txBank 2 --tx 0-71 --rxBank 1 --rx 0-71 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12
+#mp7butler.py -c $CONN easylatency $address --timeout $TIMEOUT --txBank 2 --tx 1 --rxBank 1 --rx 0-3 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12
+#mp7butler.py -c $CONN easylatency $address --timeout $TIMEOUT --txBank 2 --tx 31 --rxBank 1 --rx 50 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12
+mp7butler.py -c $CONN rosetup $address --timeout $TIMEOUT --internal --bxoffset 2
 #mp7butler.py -c $CONN rosetup $address --bxoffset 2
-mp7butler.py -c $CONN romenu $address $ROMENU_FILE standardMenu
-mp7butler.py -c $CONN zsmenu $address $ROMENU_FILE zsStandardMenu # comment this line if the ZS is configured with SWATCH
+mp7butler.py -c $CONN romenu $address --timeout $TIMEOUT $ROMENU_FILE standardMenu
+mp7butler.py -c $CONN zsmenu $address --timeout $TIMEOUT $ROMENU_FILE zsStandardMenu # comment this line if the ZS is configured with SWATCH
 #bash setup_zs.sh
 
 echo Pattern:  single >> $work_path/summary.txt
@@ -50,8 +52,8 @@ do
   mp7butler.py -c $CONN inspect $address readout.readout_zs.csr.info | grep readout.readout_zs &>> $work_path/zs_status.txt
   mp7butler.py -c $CONN inspect $address readout.readout_zs.csr.stat | grep readout.readout_zs &>> $work_path/zs_status.txt
 
-  mp7butler.py -c $CONN write $address readout.readout_zs.csr.ctrl.en 0 &>/dev/null
-  mp7butler.py -c $CONN -v roevents $address 1 --bxs $i --outputpath $tmp_path/output_nozs.dat &> $tmp_path/roevents_nozs.txt
+  mp7butler.py -c $CONN write $address --timeout $TIMEOUT readout.readout_zs.csr.ctrl.en 0 &>/dev/null
+  mp7butler.py -c $CONN -v roevents $address --timeout $TIMEOUT 1 --bxs $i --outputpath $tmp_path/output_nozs.dat &> $tmp_path/roevents_nozs.txt
 
   echo "After no ZS RO" &>> $work_path/zs_status.txt
   echo BX $i &>> $work_path/zs_status.txt
@@ -59,8 +61,8 @@ do
   mp7butler.py -c $CONN inspect $address readout.readout_zs.csr.info | grep readout.readout_zs &>> $work_path/zs_status.txt
   mp7butler.py -c $CONN inspect $address readout.readout_zs.csr.stat | grep readout.readout_zs &>> $work_path/zs_status.txt
 
-  mp7butler.py -c $CONN write $address readout.readout_zs.csr.ctrl.en 1 &>/dev/null
-  mp7butler.py -c $CONN -v roevents $address 1 --bxs $i --outputpath $tmp_path/output_zs.dat &> $tmp_path/roevents_zs.txt
+  mp7butler.py -c $CONN write $address --timeout $TIMEOUT readout.readout_zs.csr.ctrl.en 1 &>/dev/null
+  mp7butler.py -c $CONN -v roevents $address --timeout $TIMEOUT 1 --bxs $i --outputpath $tmp_path/output_zs.dat &> $tmp_path/roevents_zs.txt
 
   echo "After ZS RO" &>> $work_path/zs_status.txt
   echo BX $i &>> $work_path/zs_status.txt
--- ugmt_patterns/zs_test/zs_validation.sh
+++ ugmt_patterns/zs_test/zs_validation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 CONN=/home/utcausr/mp7sw/connection_files/connections-b40.xml
-address=ugmt
+#address=ugmt
+address=ugmt-zs-sept2017
+TIMEOUT=5000
 root_path=/home/utcausr/uGMTScripts/ugmt_patterns/zs_test
 work_path=$root_path/batch_run_`date +%Y%m%d_%H%M%S`
 tmp_path=$root_path/tmp
@@ -29,14 +31,14 @@ do
   #mp7butler.py -c $CONN reset $address --clksrc=internal # comment this line if the ZS is configured with SWATCH
 
   #inject uncompressed rx file
-  mp7butler.py -c $CONN -v xbuffers $address tx PlayOnce -e $e --inject file://$tmp_path/uncompressed.txt
+  mp7butler.py -c $CONN -v xbuffers $address tx PlayOnce -e $e --timeout $TIMEOUT --inject file://$tmp_path/uncompressed.txt
   mp7butler.py -c $CONN txmgts -e $e --loopback --pattern=none $address
   mp7butler.py -c $CONN rxmgts -e $e $address
   mp7butler.py -c $CONN rxalign -e $e --to-bx 7,5 $address
-  mp7butler.py -c $CONN easylatency $address --txBank 2 --tx 0-23,24-31 --rxBank 1 --rx 36-71 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12
-  mp7butler.py -c $CONN rosetup $address --internal --bxoffset 2
-  mp7butler.py -c $CONN romenu $address $ROMENU_FILE standardMenu
-  mp7butler.py -c $CONN zsmenu $address $ROMENU_FILE zsStandardMenu # comment this line if the ZS is configured with SWATCH
+  mp7butler.py -c $CONN easylatency $address --timeout $TIMEOUT --txBank 2 --tx 0-23,24-31 --rxBank 1 --rx 36-71 --algoLatency 27 --masterLatency 37 --rxExtraFrames 12 --txExtraFrames 12
+  mp7butler.py -c $CONN rosetup $address --timeout $TIMEOUT --internal --bxoffset 2
+  mp7butler.py -c $CONN romenu $address --timeout $TIMEOUT $ROMENU_FILE standardMenu
+  mp7butler.py -c $CONN zsmenu $address --timeout $TIMEOUT $ROMENU_FILE zsStandardMenu # comment this line if the ZS is configured with SWATCH
   #bash setup_zs.sh
 
   echo Pattern:  $rxfile >> $work_path/summary.txt
@@ -52,8 +54,8 @@ do
     mp7butler.py -c $CONN inspect $address readout.readout_zs.csr.stat | grep readout.readout_zs &>> $work_path/zs_status.txt
 
     #disable zs menu
-    mp7butler.py -c $CONN write $address readout.readout_zs.csr.ctrl.en 0 &>/dev/null
-    mp7butler.py -c $CONN -v roevents $address 1 --bxs $i --outputpath $tmp_path/output_nozs.dat &> $tmp_path/roevents_nozs.txt
+    mp7butler.py -c $CONN write $address --timeout $TIMEOUT readout.readout_zs.csr.ctrl.en 0 &>/dev/null
+    mp7butler.py -c $CONN -v roevents $address 1 --bxs $i --timeout $TIMEOUT --outputpath $tmp_path/output_nozs.dat &> $tmp_path/roevents_nozs.txt
 
     echo After no ZS RO &>> $work_path/zs_status.txt
     echo BX $i &>> $work_path/zs_status.txt
@@ -62,18 +64,18 @@ do
     mp7butler.py -c $CONN inspect $address readout.readout_zs.csr.stat | grep readout.readout_zs &>> $work_path/zs_status.txt
 
     #enable zs menu and check whether it is enabled (there were some problems with this)
-    mp7butler.py -c $CONN write $address readout.readout_zs.csr.ctrl.en 1 &>/dev/null
+    mp7butler.py -c $CONN write $address --timeout $TIMEOUT readout.readout_zs.csr.ctrl.en 1 &>/dev/null
     en="$(mp7butler.py -c $CONN -l inspect $address readout.readout_zs.csr.ctrl.en)"
 
     until [ ${en:(-1)} == 1 ]
     do
-      mp7butler.py -c $CONN write $address readout.readout_zs.csr.ctrl.en 1 &>/dev/null
+      mp7butler.py -c $CONN write $address --timeout $TIMEOUT readout.readout_zs.csr.ctrl.en 1 &>/dev/null
       #mp7butler.py -c $CONN inspect $address readout.readout_zs.csr.ctrl.val_mode
       en="$(mp7butler.py -c $CONN -l inspect $address readout.readout_zs.csr.ctrl.en)"
       echo Zero suppression status ${en:(-1)}
     done
 
-    mp7butler.py -c $CONN -v roevents $address 1 --bxs $i --outputpath $tmp_path/output_zs.dat &> $tmp_path/roevents_zs.txt
+    mp7butler.py -c $CONN -v roevents $address 1 --bxs $i --timeout $TIMEOUT --outputpath $tmp_path/output_zs.dat &> $tmp_path/roevents_zs.txt
 
     echo After ZS RO &>> $work_path/zs_status.txt
     echo BX $i &>> $work_path/zs_status.txt
